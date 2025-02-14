7344070b4245b20afe62d50508b94a19951301ff
soyung sim <simsy1024@gmail.com>
2025-02-14 20:13:42 +0900

    [Rename] import 파일 수정

---
+++ src/components/layouts/ActionButtons.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import RandomJoinButton from "../ui/gameListPage/gameRoomList/RandomJoinButton";
+import CreateRoomButton from "../ui/gameListPage/gameRoomList/CreateRoomButton";
+
+interface ActionButtonsProps {
+  onRandomJoin: () => void;
+  onCreateRoom: () => void;
+}
+
+const ActionButtons: React.FC<ActionButtonsProps> = ({
+  onRandomJoin,
+  onCreateRoom,
+}) => {
+  return (
+    <>
+      <RandomJoinButton onJoin={onRandomJoin} />
+      <CreateRoomButton onCreate={onCreateRoom} />
+    </>
+  );
+};
+
+export default ActionButtons;
