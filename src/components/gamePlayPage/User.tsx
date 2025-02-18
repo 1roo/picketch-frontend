a9dd027f39d934d7d4abb6df4f8cef0e000bdea7
hanaru <133945249+1roo@users.noreply.github.com>
2025-02-18 14:23:42 +0900

    [FEAT] 게임 그림그리는 컴포넌트 작성

---
+++ src/components/gamePlayPage/User.tsx
@@ -0,0 +1,14 @@
+export default function User() {
+  return (
+    <div>
+      <div>
+        <img alt="유저캐릭터"></img>
+      </div>
+      <div>
+        <p>닉네임</p>
+        <p>점수</p>
+        <p>지역</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/GamePlayPage.tsx
@@ -0,0 +1,9 @@
+import GameDrawing from "../components/gamePlayPage/GameDrawing";
+
+export default function GamePlayPage() {
+  return (
+    <div>
+      <GameDrawing />
+    </div>
+  );
+}
