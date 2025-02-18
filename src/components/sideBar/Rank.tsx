44b012ce1b54ced96b56a2e2117d1fbf124ff33e
hanaru <133945249+1roo@users.noreply.github.com>
2025-02-18 11:28:13 +0900

    [DESIGN] sidebar 랭킹 ui

--- src/components/sideBar/Rank.tsx
+++ src/components/sideBar/Rank.tsx
@@ -1,9 +1,33 @@
 import * as S from "../../styles/sideBar";
 
 export default function Rank() {
+  const totalRankPercentage = 40;
+  const districtRankPercentage = 20;
+
   return (
     <S.RankDiv>
-      <p>랭킹</p>
+      <p>내 랭킹</p>
+      <S.RankContainer>
+        <S.RankItem>
+          <span>
+            전체 상위 <b>40%</b>
+          </span>
+          <S.TriangleWrapper>
+            <S.CylinderBase />
+            <S.CylinderFill fillPercentage={100 - totalRankPercentage} />
+          </S.TriangleWrapper>
+        </S.RankItem>
+
+        <S.RankItem>
+          <span>
+            구 상위 <b>20%</b>
+          </span>
+          <S.TriangleWrapper>
+            <S.CylinderBase />
+            <S.CylinderFill fillPercentage={100 - districtRankPercentage} />
+          </S.TriangleWrapper>
+        </S.RankItem>
+      </S.RankContainer>
     </S.RankDiv>
   );
 }
