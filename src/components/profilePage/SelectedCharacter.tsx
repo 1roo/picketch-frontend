7b1bf1f4abc529f60e2391607456ffd47ff596e6
soyung sim <simsy1024@gmail.com>
2025-02-19 14:13:12 +0900

    [Fix] SelectedImage src updata

--- src/components/profilePage/SelectedCharacter.tsx
+++ src/components/profilePage/SelectedCharacter.tsx
@@ -15,7 +15,7 @@ const SelectedCharacter: React.FC<Props> = ({ selectedCharacter }) => {
     <SelectedCharacterWrapper>
       <SelectedCharacterText>선택 캐릭터</SelectedCharacterText>
       <SelectedCharacterContainer>
-        <SelectedImage src="/images/chicken.png" alt="선택된 캐릭터" />
+        <SelectedImage src={selectedCharacter} alt={selectedCharacter} />
       </SelectedCharacterContainer>
     </SelectedCharacterWrapper>
   );
