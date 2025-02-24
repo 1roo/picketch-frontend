759c4bfea137fe38a713bb35931d024021ab051f
1roo <93hanaru@gmail.com>
2025-02-24 17:43:43 +0900

    [FEAT] 게임방 입장 소켓 연결

--- src/interfaces/gameRoom.ts
+++ src/interfaces/gameRoom.ts
@@ -3,6 +3,7 @@ export interface GameRoom {
   roomName: string; // 방 제목
   isPrivate: boolean; // 비밀방 여부
   playerCount: number; // 플레이어 수
+  onClick: () => void;
 }
 
 // export interface GameRoom {
